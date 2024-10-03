20573df40213ca70709b7a64e1cc68209629a455
zawa-kun <shult.0219@gmail.com>
2024-10-03 23:37:18 +0900

    環境構築・ルーティング設定

--- selfboost-frontend/src/App.js
+++ selfboost-frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Register/>}/>
+        <Route path="/register" element={<Login/>}/>
+        <Route path="/profile/:id" element={<Profile/>}/>
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
